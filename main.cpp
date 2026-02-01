a2365d66c3edebdadee813163d6a01a0b6f5b6b5
Hala E. <halaelhanafy119@gmail.com>
2026-02-01 17:34:38 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,74 @@
+/* this is for a multi line comment */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+
+   cout << "My name is Hala!"; //to display output to the screen (this is for a single line comment)
+
+   cout << endl;
+   cout << "My Major is Computer Science and Cybersecurity."; // you can add << endl; right next to the code to make it look better, take out semicolon after cybersecurity
+   // u can add \n as an endl theyre the same thing
+   // its <<"n"; or "Hello world\n";
+   //u can also do cout << "Hello world\nHello world\n";
+   //if u wanna add space between each sentence horizontally add << endl; again
+   cout << endl;
+   cout << "I am a freshman!";
+   cout << endl;
+   cout << "Student";
+   cout << endl;
+   cout << "I enjoy reading as a hobby.";
+   cout << endl;
+   cout << "My favorite food in NYC is Chipotle!";
+   cout << endl;
+   cout << "My favorite drink on campus is boba tea!";
+   cout << endl;
+   cout << "Some fun facts about me are that I love cats, I love the winter, and I am a big foodie!";
+   cout << endl;
+
+  // int age {0};
+
+ //  cout << "Please enter your age: ";
+
+//   cin >> age ;
+
+  // cout << "Your age: " << age << endl;
+
+   //get three numbers from the user 
+//   float num1{0}, num2{0}, num3{0};
+
+   // we use a float variable for numbers but if we wanted them to put in their name we would put in the string variable
+
+  // cout << "Enter three numbers, with decimal point: ";
+
+ //  cin >> num1 >> num2 >> num3;
+
+ //  cout << "num1: " << num1 << endl;
+  // cout << "num2: " << num2 << endl;
+  // cout << "num3: " << num3 << endl;
+
+   // or cin>> num1;
+   //cin>> num2;
+   //cin>> num3;
+
+  // cout << "Names\tAge\tNickname\nJerry\t17\tSmarty\"\n";
+   // cout << "Fiona\t19\t\"Poker Face\"\n";
+
+   int num{99};
+
+   cout << num << " is the total score" << endl;
+
+   cout << "The value stored in the variable is " << num << endl;
+
+   cout << "Number: " << num << endl;   
+
+   cout << "I attend ";
+   cout << "John Jay ";
+   cout << "College of";
+   cout << " Criminal Justice";
+
+   return 0; 
+
+} 
