44af1e330e2585ff54361af349a0f478bc4123f3
Hala Elhanafy <halaelhanafy119@gmail.com>
2026-03-03 22:03:03 -0500

    Update Class (4).h

--- Lesson 4 - Classes and Objects/Class (4).h
+++ Lesson 4 - Classes and Objects/Class (4).h
@@ -42,3 +42,25 @@ class Car{
             cout << "accelerate engine" << endl;
         }
 };
+
+#include "Class.h"
+
+int main(){
+    Car car1;
+    Car car2("Miata", "Civic", 1999);
+    Car car3 (car2); 
+    
+    Car* pCar1 = new Car; 
+    Car* pCar2 = new Car ("Toyota", "Corolla", 2025); 
+    
+    Car car[20]; 
+    
+    car2.startEngine();
+    car1.startEngine();
+    car3.startEngine();
+    
+    pCar1 -> startEngine(); 
+    pCar2 -> startEngine();
+    
+    car[19].startEngine();
+}
