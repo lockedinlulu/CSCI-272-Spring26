bbd0b477d0fdf62d614d5bf74556b34c79e2c1c0
Hala Elhanafy <halaelhanafy119@gmail.com>
2026-03-03 21:35:19 -0500

    Update Class (1).h

--- Lesson 4 - Classes and Objects/Class (1).h
+++ Lesson 4 - Classes and Objects/Class (1).h
@@ -1,3 +1,12 @@
+/*
+    Name: Hala Elhanafy
+    Course: CSCI 272
+    Lesson Number: Lesson 4
+    Code Topic: Car Class
+    Date: 03/03/2026
+*/
+
+
 #include <iostream>
 #include <string>
 using namespace std;
