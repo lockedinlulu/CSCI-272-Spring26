3af14bf3c25ac30b9bb60b70087d465e3f0f8d85
Hala Elhanafy <halaelhanafy119@gmail.com>
2026-03-03 21:53:36 -0500

    Update Class.h

--- Lesson 4 - Classes and Objects/Class.h
+++ Lesson 4 - Classes and Objects/Class.h
@@ -15,3 +15,20 @@ class class{
         
     }
 };
+
+/* Reflection:
+What I understood: I understand the skeleton of a class, so essentially what every single class consists of, how to make one, and the different class types. 
+I also understand that some classes can be public, which means it is accesible by anyone, while some are private; only accesible through that class.
+
+Difficulties: I didn't really understand why a function needed to be there however I understood it really depends on the class, it's type, and what exactly it's meant to do.
+    
+Research: I went to google and searched up other default examples of classes and tried to look for easier ways to understand, such as through YouTube videos, which
+I found extremely helpful.
+    
+AI Usage:
+I did not use AI for this portion of the code.
+
+What I learned:
+ I learned what a class skeleton looks like, so essentially what the blueprint for every class is supposed to look like, and I also learned the two different types
+ of clases, public and private, and what roles they play in regards to classes' accesibility and visibility!
+*/
