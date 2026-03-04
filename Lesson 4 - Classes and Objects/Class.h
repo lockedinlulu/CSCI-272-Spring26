735564b930f651b423514e99462ca13571699a0e
Hala Elhanafy <halaelhanafy119@gmail.com>
2026-03-03 21:36:07 -0500

    Update Class.h

--- Lesson 4 - Classes and Objects/Class.h
+++ Lesson 4 - Classes and Objects/Class.h
@@ -1,3 +1,11 @@
+/*
+    Name: Hala Elhanafy
+    Course: CSCI 272
+    Lesson Number: Lesson 4
+    Code Topic: Default Class Skeleton
+    Date: 03/03/2026
+*/
+
 class class{
     private:
     dataType dataName;
